0165e4e79369cde63453e7a35f3cb953f265a164
Luiz Henri <luizhenri26032007@gmail.com>
2026-09-14 18:10:28 -0300

    feat: adiciona novas analises com pandas

--- main.py
+++ main.py
@@ -52,37 +52,19 @@ def calcular_ticket_medio(df):
 def encontrar_maior_venda(df):
     indice = df["valor"].idxmax()
 
-    return df.loc[indice, "valor"]
+    produto = df.loc[indice, "produto"]
+    valor = df.loc[indice, "valor"]
+
+    return produto, valor
 
 
 def encontrar_menor_venda(df):
     indice = df["valor"].idxmin()
 
-    return df.loc[indice, "valor"]
+    produto = df.loc[indice, "produto"]
+    valor = df.loc[indice, "valor"]
 
-
-def encontrar_produto_maior_venda(vendas):
-    maior_venda = vendas[0]["valor"]
-    produto_maior_venda = vendas[0]["produto"]
-
-    for venda in vendas:
-        if venda["valor"] > maior_venda:
-            maior_venda = venda["valor"]
-            produto_maior_venda = venda["produto"]
-
-    return produto_maior_venda, maior_venda
-
-
-def encontrar_produto_menor_venda(vendas):
-    menor_venda = vendas[0]["valor"]
-    produto_menor_venda = vendas[0]["produto"]
-
-    for venda in vendas:
-        if venda["valor"] < menor_venda:
-            menor_venda = venda["valor"]
-            produto_menor_venda = venda["produto"]
-
-    return produto_menor_venda, menor_venda
+    return produto, valor
 
 
 # Carregamento dos dados
@@ -91,24 +73,43 @@ vendas, erros = carregar_vendas()
 # Criação do DataFrame
 df = pd.DataFrame(vendas)
 
-# Análises
+# ANÁLISES GERAIS
+
 faturamento = calcular_faturamento(df)
 quantidade_vendas = len(df)
 ticket_medio = calcular_ticket_medio(df)
 
-maior_venda = encontrar_maior_venda(df)
-menor_venda = encontrar_menor_venda(df)
+produto_maior, valor_maior = encontrar_maior_venda(df)
+produto_menor, valor_menor = encontrar_menor_venda(df)
 
-produto_maior, valor_maior = encontrar_produto_maior_venda(vendas)
-produto_menor, valor_menor = encontrar_produto_menor_venda(vendas)
+# ANÁLISES COM PANDAS
 
+# Vendas ordenadas da maior para a menor
+vendas_ordenadas = df.sort_values("valor", ascending=False)
 
-# Resultados
-print(f"Faturamento total: R$ {faturamento}")
+# 3 maiores vendas
+top_3_vendas = vendas_ordenadas.head(3)
+
+# Resumo por produto
+resumo_produtos = df.groupby("produto").agg(
+    quantidade_vendas=("valor", "size"),
+    faturamento=("valor", "sum")
+)
+
+# Print dos Resultados
+
+print("\n RESUMO GERAL ")
+print(f"Faturamento total: R$ {faturamento:.2f}")
 print(f"Quantidade de vendas: {quantidade_vendas}")
 print(f"Ticket médio: R$ {ticket_medio:.2f}")
-print(f"Maior venda: R$ {maior_venda}")
-print(f"Menor venda: R$ {menor_venda}")
-print(f"Produto com maior venda: {produto_maior} - R$ {valor_maior}")
-print(f"Produto com menor venda: {produto_menor} - R$ {valor_menor}")
+print(f"Maior venda: {produto_maior} - R$ {valor_maior:.2f}")
+print(f"Menor venda: {produto_menor} - R$ {valor_menor:.2f}")
 print(f"Registros inválidos: {erros}")
+
+
+print("\n TOP 3 VENDAS ")
+print(top_3_vendas)
+
+
+print("\n RESUMO POR PRODUTO ")
+print(resumo_produtos)
